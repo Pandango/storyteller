1198d3b64f7c2146131d210581d0d54c7f898c41
Pandango <littlepanda.daisuki@gmail.com>
2016-05-22 23:46:40 +0700

    v2
    เพิ่มหน้า article และ หน้า home

---
+++ article.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		
+		<link rel="stylesheet" type="text/css" href="style_sheet_panda.css">
+
+		<!-- Jquery-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	    <!-- Custom Fonts -->
+	    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css"
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+		
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+		<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+		<script src="semantic/semantic.min.js"></script>
+		<script src="carousel.js"></script>
+		<title></title>
+	</head>
+	<body>
+
+
+	<div id="container">
+		<!-- Header -->
+		<div class="nav nav-bg">
+			<div class="w-con">
+				<div class="col-md-2"> LOGO </div>
+				<div class="col-md-1"></div>
+				<div class="col-md-4 nav-search-con">
+					<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+					<input type="text" class="nav-search" placeholder="Search...">
+				</div>
+				<div class="col-md-2"></div>
+				<div class="col-md-1">
+					<a href="" class="nav-link">Home</a>
+				</div>
+				<div class="col-md-1 dropdown">
+					<a href="" class="nav-link drop-btn">Article</a>
+					<div class="dropdown-content">
+						<a href="">Experience</a>
+						<a href="">Short Story</a>
+						<a href="">Review</a>
+						<a href="">Knowledge</a>
+					</div>
+				</div>
+				<div class="col-md-1">
+					<a href="" class="nav-link">Login</a>
+				</div>
+			</div>
+		</div>
+		<!-- Content -->
+		<div class="w-con main-container" id="body">
+		
+		<h3 style="margin-top:30px;">Experience</h3>
+		<hr>					
+		<!--card story-->
+		<div class="content-card-latest">					
+				<div style="float:left;">
+					<img style="width:300px; height:100% postion:relative;" src="images\articleCover\default-cover.png">
+				</div>
+				
+				<div style="float:left; position:relative; width:560px; margin-top:20px; padding-bottom:20px; margin:20px;">
+				<div class="content-genre">
+					<i class="fa fa-flag" aria-hidden="true"></i>
+					<span style="padding-left:5px;">experience</span>
+				</div>
+					<div>
+						<h3>Cute dogs</h3>
+						<span>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachingsborn and I will give you a complete account of the system, and expound the actual teachingsBut I must explain to you </span>
+						<hr>
+					</div>	
+
+					<div style="position:absolute; clear: both; float: left; padding-bottom:20px; bottom:0px;">
+						<div style="display:inline;"><img class="ui avatar image" src="images\user\default.png">Matt</div>
+					</div>	
+					<div style="bottom:0px; text-align:right; padding:5px;">
+							Joined in 2014
+					</div>																		
+				</div>				
+		</div>
+		<!--card story-->
+		<div class="content-card-latest">					
+				<div style="float:left;">
+					<img style="width:300px; height:100% postion:relative;" src="images\articleCover\default-cover.png">
+				</div>
+				
+				<div style="float:left; position:relative; width:560px; margin-top:20px; padding-bottom:20px; margin:20px;">
+				<div class="content-genre">
+					<i class="fa fa-book" aria-hidden="true"></i>
+					<span style="padding-left:5px;">Short story</span>
+				</div>
+					<div>
+						<h3>Cute dogs</h3>
+						<span>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachingsborn and I will give you a complete account of the system, and expound the actual teachingsBut I must explain to you </span>
+						<hr>
+					</div>	
+
+					<div style="position:absolute; clear: both; float: left; padding-bottom:20px; bottom:0px;">
+						<div style="display:inline;"><img class="ui avatar image" src="images\user\default.png">Matt</div>
+					</div>	
+					<div style="bottom:0px; text-align:right; padding:5px;">
+							Joined in 2014
+					</div>																		
+				</div>				
+		</div>		
+		<!--card story-->
+		<div class="content-card-latest">					
+				<div style="float:left;">
+					<img style="width:300px; height:100% postion:relative;" src="images\articleCover\default-cover.png">
+				</div>
+				
+				<div style="float:left; position:relative; width:560px; margin-top:20px; padding-bottom:20px; margin:20px;">
+				<div class="content-genre">
+					<i class="fa fa-eye" aria-hidden="true"></i>
+					<span style="padding-left:5px;">Review</span>
+				</div>
+					<div>
+						<h3>Cute dogs</h3>
+						<span>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachingsborn and I will give you a complete account of the system, and expound the actual teachingsBut I must explain to you </span>
+						<hr>
+					</div>	
+
+					<div style="position:absolute; clear: both; float: left; padding-bottom:20px; bottom:0px;">
+						<div style="display:inline;"><img class="ui avatar image" src="images\user\default.png">Matt</div>
+					</div>	
+					<div style="bottom:0px; text-align:right; padding:5px;">
+							Joined in 2014
+					</div>																		
+				</div>				
+		</div>
+				<!--card story-->
+		<div class="content-card-latest">					
+				<div style="float:left;">
+					<img style="width:300px; height:100% postion:relative;" src="images\articleCover\default-cover.png">
+				</div>
+				
+				<div style="float:left; position:relative; width:560px; margin-top:20px; padding-bottom:20px; margin:20px;">
+				<div class="content-genre">
+					<i class="fa fa-flask" aria-hidden="true"></i>
+					<span style="padding-left:5px;">Knowledge</span>
+				</div>
+					<div>
+						<h3>Cute dogs</h3>
+						<span>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachingsborn and I will give you a complete account of the system, and expound the actual teachingsBut I must explain to you </span>
+						<hr>
+					</div>	
+
+					<div style="position:absolute; clear: both; float: left; padding-bottom:20px; bottom:0px;">
+						<div style="display:inline;"><img class="ui avatar image" src="images\user\default.png">Matt</div>
+					</div>	
+					<div style="bottom:0px; text-align:right; padding:5px;">
+							Joined in 2014
+					</div>																		
+				</div>				
+		</div>	
+		<button type="button" id="btn-readmore" class="btn-center btn btn-default" >Read More...</button>		
+	</div>
+		
+
+	<!-- Footer -->
+		<div class="footer">
+			<div class="w-con footer-container">
+				<div class="col-md-3">
+					<span>
+						<a href="" class="footer-link">Home </a>	
+					</span>
+					<span class="text-footer"> /</span>
+					<span>
+						<a href="" class="footer-link">Article</a>
+					</span>
+				</div>
+				<div class="col-md-7"></div>
+				<div class="col-md-2" style="text-align: right; padding-right: 0px;">
+					<a href="" class="footer-btn">
+						<i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+					</a>&nbsp;
+					<a href="" class="footer-btn">
+						<i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i>
+					</a>					
+				</div>
+				
+			</div>
+			<div class="w-con">
+				<div class="col-md-3">
+					<p class="text-footer-miner">Copyright (c) 2016 </p>
+				</div>
+				<div class="col-md-6"></div>
+				<div class="col-md-1" style="text-align: right;">
+					<i class="fa fa-envelope-o fa-2x" style="color: #FCE8E1;" aria-hidden="true"></i>
+				</div>
+				<div class="col-md-2" style="padding-left: 0px;">
+					<a href="" class="text-footer-miner footer-btn">something@mail.com </a>
+				</div>
+			</div>
+			
+		</div>
+	</body>
+
+
+</html>
